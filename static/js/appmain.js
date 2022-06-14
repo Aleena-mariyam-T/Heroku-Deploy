e022d51845bda0c90f1740ae3d1200e560445829
--global <aleena.thankappan@sayonetech.com>
2022-06-14 17:26:53 +0530

    deployment error

--- static/js/appmain.js
+++ static/js/appmain.js
@@ -2,16 +2,20 @@ jQuery(function ($) {
     'use strict';
     $(document).ready(async function () {
 
-        $('#eventsubmit').click(function(){
+        $('#cancel').click(function(){
+            var result = confirm("Are you sure ?")
+            console.log("hihi");
+        })
+        $('#eventlist').click(function(){
             var result = confirm("for publishing event you need to pay?");
             console.log("insude event submit");
-            if (result){
-                $.ajax({
-                    method : 'GET',
-                    url :"'{% 'CheckPaymentView' %}",
-                })
+            // if (result){
+            //     $.ajax({
+            //         type: 'get',
+            //         url: decodeURI('{% event/CheckPaymentView %}'),
+            //     })
                 
-            }
+            // }
         });
         
         $('#id_event_start_date').datepicker({
